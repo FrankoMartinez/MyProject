8cdcde1054877da8d9b1c218d7e7432c0ae8c8f0
Francisco <frankmartinez4050@gmail.com>
2021-11-17 03:58:00 -0600

    Completed

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
-// Fetch refrences
-// fetch('http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=281a3daeda2eda2ac74da3793997eb82')
-// .then(resp => resp.json())
-// .then(res => console.log(res))
-
+document.addEventListener('DOMContentLoaded', () => {
 // Event listener when I press enter it runs the function with the fetch
 const search = document.querySelector('input#search-box')
 search.addEventListener('keypress', function (event) {
@@ -20,7 +16,7 @@ function keyPress() {
         document.querySelector('div.city').innerHTML = res.name
         document.querySelector('div.temp').innerHTML = Math.round(res.main.temp) + '&deg'
         // set the weather query selector to a variable to capitalize the first letter
-        document.querySelector('div.weather').innerHTML  = res.weather[0].description[0].toUpperCase() + res.weather[0].description.slice(1)
+        document.querySelector('div.weather').innerHTML = res.weather[0].description[0].toUpperCase() + res.weather[0].description.slice(1)
         //days of week array to get current day 
         const d = new Date();
         
@@ -36,4 +32,14 @@ function keyPress() {
         let day = weekday[d.getDay()];
         // displays the day of the week
         document.querySelector('div.day').innerHTML = day
-})}
+        // Clear button
+        const button = document.querySelector('button')
+        button.addEventListener('click', function () {
+            document.querySelector('input#search-box').value = ''
+            document.querySelector('div.city').innerHTML = ''
+            document.querySelector('div.temp').innerHTML = ''
+            document.querySelector('div.weather').innerHTML = ''
+            document.querySelector('div.day').innerHTML = ''
+        })
+    })}
+})
